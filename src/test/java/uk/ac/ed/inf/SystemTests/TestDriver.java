b3bd80dbb487547f5f67f0e61e8ce03c9ccb1499
IainHigh <iain.high@sky.com>
2024-09-24 10:56:56 +0100

    Renaming test driver so it is caught by Surefire

--- src/test/java/uk/ac/ed/inf/SystemTests/TestDriver.java
+++ src/test/java/uk/ac/ed/inf/SystemTests/TestDriver.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.time.Instant;
 import java.time.Duration;
 
-public class Driver {
+public class TestDriver {
     public static void main(String[] args) {
 
         String[] dates = generateAllDates();
