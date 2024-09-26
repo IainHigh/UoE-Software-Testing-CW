a3c801187c7ae2d5540d2e8692b4d66f6ff45c6a
IainHigh <iain.high@sky.com>
2024-09-26 09:42:43 +0100

    Modifying the SystemTest so that all dates are counted as seperate tests

--- src/test/java/uk/ac/ed/inf/SystemTests/SystemTest.java
+++ src/test/java/uk/ac/ed/inf/SystemTests/SystemTest.java
@@ -4,41 +4,48 @@ import uk.ac.ed.inf.PizzaDrone;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.time.Instant;
-
 import java.time.Duration;
 
-// TODO: Probably seperate each date into it's own test case, so if one fails, the others can still run. And we can see the results.
-
+@RunWith(Parameterized.class)
 public class SystemTest {
 
-    @Test
-    public void testAllDates() {
-        // This test runs the program with all possible dates. If an exception is thrown, there is a bug.
+    private String date;
 
-        String[] dates = generateAllDates();
-        for (String date : dates) {
-            System.out.println("Testing date: " + date);
-            Instant start = Instant.now();
-
-            try{
-                PizzaDrone.main(new String[]{date, "https://ilp-rest.azurewebsites.net/"});
-                // Try running the program with the given date. If an exception is thrown, there is a bug.
-            }
-            catch (Exception e){
-                System.out.println("An exception was thrown: " + e.getMessage());
-                assert(false);
-            }
-
-            System.out.println("\tTime taken: " + Duration.between(start, Instant.now()).toMillis() + "ms");
-            
-        }
+    public SystemTest(String date) {
+        this.date = date;
     }
 
-    private static String[] generateAllDates(){
+    @Parameters
+    public static Collection<String> dates() {
+        return generateAllDates();
+    }
+
+    @Test
+    public void testDate() {
+        System.out.println("Testing date: " + date);
+        Instant start = Instant.now();
+
+        try{
+            PizzaDrone.main(new String[]{date, "https://ilp-rest.azurewebsites.net/"});
+        }
+        catch (Exception e){
+            System.out.println("An exception was thrown: " + e.getMessage());
+            assert(false);
+        }
+
+        // Assert the time taken in seconds is less than 60
+        assert(Duration.between(start, Instant.now()).getSeconds() < 60);
+    }
+
+    private static List<String> generateAllDates(){
         List<String> dates = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 31; j++) {
@@ -48,6 +55,6 @@ public class SystemTest {
                 dates.add("2023-" + String.format("%02d", i) + "-" +String.format("%02d", j));
             }
         }
-        return dates.toArray(new String[0]);
+        return dates;
     }
 }
