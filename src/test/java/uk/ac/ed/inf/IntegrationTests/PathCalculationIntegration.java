6e757cf88ae353f4390d5d77f20ee8e9c06ce6f3
IainHigh <iain.high@sky.com>
2024-10-10 11:25:17 +0100

    Adding more tests

---
+++ src/test/java/uk/ac/ed/inf/IntegrationTests/PathCalculationIntegration.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.inf.IntegrationTests;
+
+import org.junit.Test;
+
+public class PathCalculationIntegration {
+
+    @Test
+    public void testDate(){
+        assert(true);
+    }
+}
